0df79be5bea4f01c0f0d349b737a0b1bfc836b35
Yoonseo Nam <turtlenam0928@gmail.com>
2020-06-30 17:58:25 +0900

    feat: 날씨 정보를 가져오는 기능 구현
    - API KEY를 담은 파일 생성 및 .gitignore에 추가
    - axios를 통해 날씨 정보를 가져옴

--- App.js
+++ App.js
@@ -2,14 +2,30 @@ import React, { useEffect, useState } from 'react';
 import { Alert } from 'react-native';
 import Loading from './Loading';
 import * as Location from 'expo-location';
+import * as secret from './secret';
+import axios from 'axios';
 
 export default function App() {
   const [isLoading, setIsLoading] = useState(true);
+
+  const BASE_URL = 'http://api.openweathermap.org/data/2.5/weather';
+  const API_KEY = secret.KEY;
+
+  const getWeather = async (lat, lon) => {
+    const { data } = await axios.get(BASE_URL, {
+      params: {
+        lat, lon, appid: API_KEY
+      }
+    });
+    console.log(data);
+  }
 
   const getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
       const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
+      await getWeather(latitude, longitude);
+      setIsLoading(false);
     }
     catch (e) {
       Alert.alert('Can\'t find you', 'So sad :(');
---
+++ secret.js
@@ -0,0 +1,1 @@
+export const KEY = 'f91100a4bc6634ec918e8ffae45a0441';
