ce5394d5b7ab896bc3e9e02cf38db57cf30e9820
Yoonseo Nam <turtlenam0928@gmail.com>
2020-06-30 16:51:34 +0900

    feat: 위치 정보를 가져오는 기능 구현

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
 import Loading from './Loading';
+import * as Location from 'expo-location';
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const getLocation = async () => {
+    try {
+      await Location.requestPermissionsAsync();
+      const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
+    }
+    catch (e) {
+      Alert.alert('Can\'t find you', 'So sad :(');
+    }
+  }
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
   return (
-      <Loading/>
+      isLoading ? <Loading/> : null
   );
 }
